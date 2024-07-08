89d242e507f4cae121f6d14954b48eb59f0a79b5
Maria Zavala <mariap.zavala00@gmail.com>
2024-07-08 10:40:27 -0700

    working on contact

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,9 +1,9 @@
 'use server';
 import { Resend } from "resend";
 import { validateString } from "@/lib/utils";
+import { getErrorMessage } from "@/lib/utils";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-
 
 
 // We see the info on out server now by using actions 
@@ -12,18 +12,25 @@ export const sendEmail = async (formData: FormData) => {
     const message = formData.get('message');
 
     if (!validateString(senderEmail, 500)) {
-        return {error: "invalid sender email"}
+        return {error: "invalid sender email"};
     }
 
     if (!validateString(message, 5000)) {
-        return {error: "invalid message"}
+        return {error: "invalid message"};
     }
 
-    await resend.emails.send({
-        from: "onboarding@resend.dev",
-        to: 'pazrodriguez612@gmail.com',
-        subject: 'message from contact form',
-        reply_to: senderEmail as string,
-        text: message as string
-    })
+    try {
+        await resend.emails.send({
+            from: "Contact Form <onboarding@resend.dev>",
+            to: 'pazrodriguez612@gmail.com',
+            subject: 'message from contact form',
+            reply_to: senderEmail as string,
+            text: message as string
+        })
+    } catch (error: unknown) {
+        return {
+            error: getErrorMessage(error)
+        }
+    }
+    
 };
